8610a0061254b26500aeab3ed305cbf589a5ce04
Thomas WACHE <thomas.wache@intradef.gouv.fr>
2018-12-13 23:06:25 +0100

    Fix gcc warnings due to buffer sizes

--- log.c
+++ log.c
@@ -6,7 +6,7 @@
 
 void __log_message(const char *file, int line, const char *format, ...) {
     va_list args;
-    char datetime[256];
+    char datetime[32];
     char new_format[256];
     struct tm tm;
     time_t now = time(NULL);
@@ -14,7 +14,7 @@ void __log_message(const char *file, int line, const char *format, ...) {
     va_start(args, format);
     localtime_r(&now, &tm);
     strftime(datetime, sizeof(datetime), "%F %T", &tm);
-    snprintf(new_format, sizeof(new_format), "[%s] [%s:%d] %s\n", datetime, file, line, format);
+    snprintf(new_format, sizeof(new_format) - 1, "[%s] [%s:%d] %s\n", datetime, file, line, format);
     vfprintf(stderr, new_format, args);
     va_end(args);
 }
