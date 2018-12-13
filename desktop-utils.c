a225f132c329db18b1f9b01f6662104fce216f2c
Thomas WACHE <thomas.wache@intradef.gouv.fr>
2018-12-13 23:23:31 +0100

    Revert "- clang-format"
    This reverts commit c5f056c301a107167cde3065787b1de7cf3e2037.

--- desktop-utils.c
+++ desktop-utils.c
@@ -11,14 +11,19 @@
 #include "log.h"
 #include "macros.h"
 
+
 static int daemon_handle = -1;
-static char lock_filename[PATH_MAX];
+static char lock_filename[256];
 static int in_son = false;
 static int has_lock = false;
 
 void daemon_sighandler(int signum);
+
 void __exit_daemon_uniq(void) __attribute__((destructor));
-inline bool daemonize(void) { return daemonize_uniq(NULL); }
+
+inline bool daemonize(void) {
+    return daemonize_uniq(NULL);
+}
 
 bool daemonize_uniq(const char *filename) {
 #ifdef DEBUG
@@ -33,14 +38,14 @@ bool daemonize_uniq(const char *filename) {
     sigemptyset(&sa.sa_mask);
     sigaddset(&sa.sa_mask, SIGTERM);
 
-    if (filename) {
+    if ( filename ) {
         strncpy(lock_filename, filename, sizeof(lock_filename) - 1);
         CHK_NEG(sigaction(SIGTERM, &sa, NULL));
         CHK_FALSE(has_lock = lockfile(lock_filename, &daemon_handle));
     }
 
     CHK_NEG(son = fork());
-    if (son == 0) {
+    if ( son == 0 ) {
         CHK_NEG(close(STDIN_FILENO));
         CHK_NEG(close(STDOUT_FILENO));
         CHK_NEG(close(STDERR_FILENO));
@@ -50,13 +55,13 @@ bool daemonize_uniq(const char *filename) {
         exit(EXIT_SUCCESS);
     }
 
-fail:
+    fail:
     return false;
 #endif
 }
 
 void __exit_daemon_uniq(void) {
-    if (in_son && has_lock) {
+    if ( in_son && has_lock ) {
         unlockfile(lock_filename, &daemon_handle);
         unlink(lock_filename);
     }
@@ -66,9 +71,9 @@ bool spawn(char *const cmd[], char *const envp[]) {
     pid_t son = -1;
 
     CHK_NEG(son = fork());
-    if (son == 0) {
+    if ( son == 0 ) {
         debug("Executing %s", cmd[0]);
-        if (execve(cmd[0], cmd, envp) < 0) {
+        if ( execve(cmd[0], cmd, envp) < 0 ) {
             exit(EXIT_FAILURE);
         } else {
             // Not reached
@@ -80,24 +85,18 @@ bool spawn(char *const cmd[], char *const envp[]) {
 
     return true;
 
-fail:
+    fail:
     return false;
 }
 
 bool lockfile(const char *filename, int *handle) {
     int fd;
-    char pid[8];
-    size_t pid_len;
 
-    CHK_NEG(fd = open(filename, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR));
-    pid_len = (size_t)snprintf(pid, sizeof(pid) - 1, "%d\n", getpid());
-    CHK_NEG(write(fd, (const void *)pid, pid_len));
-    handle = fd;
-
+    CHK_NEG(fd = open(filename, O_CREAT|O_EXCL|O_RDWR, S_IRUSR|S_IWUSR));
+    *handle = fd;
     return true;
 
-fail:
-    close(fd);
+    fail:
     return false;
 }
 
@@ -113,7 +112,7 @@ void daemon_sighandler(int signum) {
     debug("Caught signal %d", signum);
     UNUSED(signum);
 
-    if (in_son && has_lock) {
+    if ( in_son && has_lock ) {
         unlockfile(lock_filename, &daemon_handle);
         unlink(lock_filename);
     }
@@ -127,13 +126,13 @@ void __init(void) {
     CHK_FALSE(lockfile(LOCK_FILENAME, &handle));
     return;
 
-fail:
+    fail:
     exit(EXIT_FAILURE);
 }
 
 void __exit(void) {
     CHK_NEG(unlockfile(LOCK_FILENAME, &handle));
-fail:
+    fail:
     return;
 }
 #endif
